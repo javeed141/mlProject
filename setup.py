843598917ed44968b41e230f06f15ca410558f82
Javeed <javeedshaik7346@gmail.com>
2025-10-01 19:31:08 +0530

    after os changed

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ def get_requirements(file_path:str) -> List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        # requirements=[req.replace("\n","") for req in requirements]
+        requirements = [req.replace("\n","").replace("\r","").strip() for req in requirements]
+
         if "-e ." in requirements:
             requirements.remove("-e .")
 # setup(
