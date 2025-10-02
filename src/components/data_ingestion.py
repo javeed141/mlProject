07bb3e68aa13dfac1a4a37c3ab47db43b1daa1b4
Javeed <javeedshaik7346@gmail.com>
2025-10-02 10:53:07 +0530

    hyper parameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,7 @@
 import os
 import sys
-# from src.exception import CustomException 
-# from src.logger import logging
+from src.exception import CustomException 
+from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split 
@@ -28,10 +28,10 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         try:
             df=pd.read_csv("notebook\data\stud.csv")
-            # logging.info("Read the dataset as dataframe")
+            logging.info("Read the dataset as dataframe")
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
-            # logging.info("Train test split initiated")
+            logging.info("Train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
             
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import dill
 import pickle 
 from sklearn.metrics import r2_score
 from src.exception import CustomException 
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -19,12 +19,22 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_model(x_train,y_train,x_test,y_test,models):
+def evaluate_model(x_train,y_train,x_test,y_test,models,params):
     try:
         report={}
         for i in range(len(list(models))):
             model=list(models.values())[i]
+
+            para=params[list(models.keys())[i]]
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
+
+
+
+            # model.fit(x_train,y_train)
             y_train_pred=model.predict(x_train)
             y_test_pred=model.predict(x_test)
 
